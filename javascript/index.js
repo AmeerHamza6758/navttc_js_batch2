0bf5f5f842492652dedeccc891d39d049e4b5ee7
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2025-10-08 19:33:21 +0500

    introduction to javascript and some basic implementation

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+console.log("Javascript connected")
+
+// variables
+
+// const let var
+// 
+const myName = "Ameer Hamza"; //string
+
+let myNum = 2345678.234567 //number
+
+let isCheck = true //Boolean
+
+let isValue = undefined;
+
+console.log(myName, '1')
+
+//
+console.log(myName, '2')
+console.log(isValue, '3')
+
+
+// DataTypes
+// primitive=> string, number, Boolean, symbol, null, undefined
+
+//non-primitive
+// Array, objects, functions
