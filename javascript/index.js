7a2f459920c3fe60e7991ee8b10f521781a18c3b
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2025-10-09 18:22:44 +0500

    datatypes

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 console.log("Javascript connected")
+// dymanic typed 2-interpreted 3-cross-platform 4-single-threaded
 
 // variables
 
@@ -24,3 +25,47 @@ console.log(isValue, '3')
 
 //non-primitive
 // Array, objects, functions
+
+
+// const let var
+// block level
+{
+    const stud = "Arslan"
+    let studnum = 234567;
+    studnum = 555;
+
+    console.log(stud, 'Student name', studnum, 'student number')
+
+}
+let studnum = '876545'
+const stud = "Hamza"
+
+console.log(stud, 'Student name', studnum, 'student number')
+
+// string, number, boolean, undefined, null, symbol
+let teacherName = null;
+teacherName = "Ameer Hamza"
+
+let abc = Symbol('sdbvc')
+
+// no-primitve
+
+// Array, Object, function
+
+const studentsName = ["Ameer Hamza", "Arslan ali", "Jawad", "Karimdad", "Alizay"]
+console.log(studentsName)
+
+let employeeData = {
+    // key:value
+    name: "Ameer Hamza",
+    email: "ameer@gmail.com"
+}
+
+let studentData = {
+    // key:value
+    name: "Ali Hamza",
+    email: "ali@gmail.com"
+}
+
+
+console.log([employeeData,studentData])
